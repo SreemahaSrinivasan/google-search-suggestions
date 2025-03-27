c15f7986995a412fba013c09a34d9967d96c7b6b
SreemahaSrinivasan <srimahas2001@gmail.com>
2025-03-27 17:28:34 +0000

    code updated

--- src/components/GoogleSuggestions/index.js
+++ src/components/GoogleSuggestions/index.js
@@ -1,1 +1,68 @@
 // Write your code here
+import './index.css'
+import {Component} from 'react'
+import SuggestionItem from '../SuggestionItem'
+
+class GoogleSuggestions extends Component {
+  state = {searchInput: ''}
+
+  onChangeSearchInput = event => {
+    this.setState({
+      searchInput: event.target.value,
+    })
+  }
+
+  updateSuggestion = suggestion => {
+    this.setState({
+      searchInput: suggestion,
+    })
+  }
+
+  render() {
+    const {suggestionsList} = this.props
+    const {searchInput} = this.state
+
+    const searchedResults = suggestionsList.filter(eachSuggestion =>
+      eachSuggestion.suggestion
+        .toLowerCase()
+        .includes(searchInput.toLowerCase()),
+    )
+
+    return (
+      <div className="app-container">
+        <div className="google-suggestions-container">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/google-logo.png"
+            alt="google logo"
+          />
+          <div className="search-input-suggestions-container">
+            <div className="search-input-container">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/google-search-icon.png"
+                alt="search icon"
+              />
+              <input
+                type="search"
+                className="search-input"
+                placeholder="Search Google"
+                value={searchInput}
+                onChange={this.onChangeSearchInput}
+              />
+            </div>
+            <ul className="suggestions-list">
+              {searchedResults.map(eachSuggestion => (
+                <SuggestionItem
+                  suggestionDetails={eachSuggestion}
+                  key={eachSuggestion.id}
+                  updateSuggestion={this.updateSuggestion}
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default GoogleSuggestions
